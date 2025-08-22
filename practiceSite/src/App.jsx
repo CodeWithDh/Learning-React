88d8503a3e1b3c56b0c2a0b4565b7f62cfd6e450
Shivam <codewithdh@gmail.com>
2025-08-22 21:36:07 +0530

    Day ; 12 - Learning Advance React

--- practiceSite/src/App.jsx
+++ practiceSite/src/App.jsx
@@ -32,8 +32,18 @@ function App() {
      {/* <EffectCounter/> */}
      {/* <MaterialUi/> */}
      {/* <WeatherWidget/> */}
-
      <Counter/>
+     <div className="p-10 rounded-2xl shadow-2xl bg-white">
+        <h1 className="text-4xl font-extrabold text-blue-600 underline">
+          Tailwind is Working 🎉
+        </h1>
+        <p className="mt-4 text-lg text-gray-700">
+          If you see colors, spacing, and fonts applied → ✅ Success
+        </p>
+        <button className="mt-6 px-6 py-2 rounded-lg bg-green-500 text-white hover:bg-green-600 transition">
+          Test Button
+        </button>
+      </div>
      </>
   )
 }
