7ed6042e69449b62c102ff922ca1bde3bbab406a
Shivam <codewithdh@gmail.com>
2026-02-12 23:13:07 +0530

    I have created the currency convertor while revising React

--- currencyConvertor/src/Components/CurrencyConvertor.jsx
+++ currencyConvertor/src/Components/CurrencyConvertor.jsx
@@ -1,86 +1,58 @@
-import {react,useEffect, useState} from "react"
+import {react,useCallback,useEffect, useState} from "react"
+import { InputBox } from "./InputBox";
+import UseCurrencyInfo from "../hooks/UseCurrencyInfo";
 
 export function CurrencyConvertor(){
-    const [fromAmt,setFromAmt]=useState(0);
-    const[fromCurrency,setfromCurrency]=("INR")
-    const[toCurrency,setToCurrency]=("USD")
+    
+    const [fromAmount,setFromAmount]=useState(0);
+    const [fromCurrency,setFromCurrency]=useState("usd");
+    const [toAmount,setToAmount]=useState(0);
+    const [toCurrency,setToCurrency]=useState("inr");
+    let data=UseCurrencyInfo(fromCurrency);
+    const Convertor=useCallback((fromAmount,fromCurrency,toCurrency,data)=>{
+        return setToAmount(Math.floor(fromAmount*data[toCurrency]))
+    },[fromAmount,fromCurrency,toCurrency,setFromAmount,setToCurrency])
     return(
         <>
         {/* Currency Convertor */}
         <div
         className="bg-gray-800 w-md px-4 text-white py-4 text-center mx-auto my-8">
             {/* InputBox from */}
-            <div
-            className="bg-white w-full h-20 -my-2 mt-4 rounded-lg text-orange-500 flex justify-between px-2">
-                {/* from */}
-                <div className="from">
-                    <label 
-                    htmlFor="From"
-                    className="flex my-2 font-bold"
-                    >from</label>
-                <input 
-                type="number"
-                value={fromAmt}
-                onChange={(e)=>{
-                    setFromAmt(e.target.value)
-                }}
-                className="w-full text-orange-500 text-lg  p-1  "
-                />
-                </div>
-                {/* type */}
-                <div className="to ">
-                    <label 
-                    htmlFor="currencyType"
-                    className="flex my-2 font-bold"
-                    >Currency Type</label>
-                    <select 
-                    type="select"
-                    onChange={(e)=>{
-                        setfromCurrency(e.target.value)
-                    }}
-                    className="bg-white-800 ml-8 bg-gray-200 rounded-md p-1 text-orange-500">
-                        <option value={fromCurrency}>{fromCurrency}</option>
-                    </select>
-                </div>
-            </div>
+            <InputBox
+            label="from"
+            amount={fromAmount}
+            onAmountChange={setFromAmount}
+            selectedCurrency={fromCurrency}
+            onSelectedCurrencyChange={setFromCurrency}
+            amountIsDisabled={false}
+            currencies={Object.keys(data)}
+            />
+            
             <button
             className="bg-orange-500 p-2 rounded-lg m-0 p-0 relative "
+            onClick={(e)=>{
+                setFromCurrency(toCurrency);
+                setToCurrency(fromCurrency)
+            }}
             >Swap</button>
+
             {/* InputBox to */}
-            <div
-            className="bg-white w-full h-20 -my-2 mb-4 rounded-lg text-orange-500 flex justify-between px-2">
-                {/* from */}
-                <div className="to">
-                    <label 
-                    htmlFor="to"
-                    className="flex my-2 font-bold"
-                    >To</label>
-                <input 
-                type="number"
-                value={0}
-                disabled
-                className="w-full text-orange-500 text-lg  p-1  "
-                />
-                </div>
-                {/* Type */}
-                <div className="to ">
-                    <label 
-                    htmlFor="currencyType"
-                    className="flex my-2 font-bold"
-                    >Currency Type</label>
-                    <select 
-                    type="select"
-                    onChange={(e)=>{
-                        setToCurrency(e.target.value)
-                    }}
-                    className="bg-white-500 ml-8 bg-gray-200 rounded-md p-1 text-orange-500">
-                        <option value={toCurrency}>{toCurrency}</option>
-                    </select>
-                </div>
-            </div>
+
+            <InputBox
+            label="to"
+            amount={toAmount}
+            onAmountChange={setToAmount}
+            selectedCurrency={toCurrency}
+            onSelectedCurrencyChange={setToCurrency}
+            amountIsDisabled={true}
+            currencies={Object.keys(data)} />
+            
             <button
             className="text-white font-medium p-2 rounded-md px-6 bg-blue-600"
-            >Convert {"USD"} to {"INR"}</button>
+            onClick={(e)=>{
+                Convertor(fromAmount,fromCurrency,toCurrency,data)
+            }}
+            >Convert {fromCurrency} to {toCurrency}</button>
         </div>
         </>
     )
--- currencyConvertor/src/Components/InputBox.jsx
+++ currencyConvertor/src/Components/InputBox.jsx
@@ -1,9 +1,53 @@
-
-export function InputBox(){
+import {react,useEffect, useState} from "react"
+export function InputBox(
+    {
+        label,
+        amount,
+        onAmountChange,
+        selectedCurrency,
+        onSelectedCurrencyChange,
+        amountIsDisabled,
+        currencies=[]
+    }){
     return(
+        <>
         <div
-        className="w-md bg-white">
-
-        </div>
+            className="bg-white w-full h-20 my-2 mt-4 rounded-lg text-orange-500 flex justify-between px-2">
+                <div className="from">
+                    <label 
+                    htmlFor={label}
+                    className="flex my-2 font-bold"
+                    >{label}</label>
+                <input 
+                type="number"
+                value={amount}
+                disabled={amountIsDisabled}
+                onChange={(e)=>{
+                    onAmountChange && onAmountChange(e.target.value)
+                }}
+                className="w-full text-orange-500 text-lg  p-1  "
+                />
+                </div>
+                {/* type */}
+                <div className="to">
+                    <label 
+                    htmlFor="currency"
+                    className="flex my-2 ont-bold"
+                    >Currency</label>
+                    <select 
+                    type="select"
+                    value={selectedCurrency}
+                    onChange={(e)=>{
+                        onSelectedCurrencyChange && onSelectedCurrencyChange(e.target.value)
+                    }}
+                    className="bg-white-800 ml-8 bg-gray-200 rounded-md p-1 text-orange-500">
+                        {currencies.map((option)=>{
+                            return <option key={option} value={option}>{option}</option>
+                        })}
+                    </select>
+                </div>
+            </div>
+        </>
+        
     )
 }
